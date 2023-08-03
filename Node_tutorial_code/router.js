88d49ab141cce2d422859888d92b2d78de404997
Fangwei Jiang <emberspirit2018@gmail.com>
2023-08-04 05:25:55 +1000

    update node_tutorial.md

--- Node_tutorial_code/router.js
+++ Node_tutorial_code/router.js
@@ -1,19 +1,18 @@
-const express = require("express");
-const router = express.Router();
-const fs = require("fs");
-const message_body = require("./messages.json");
-const messages = require("./messages.json").messages;
+const express = require("express"),
+    router = express.Router(),
+    fs = require("fs"),
+    message_body = require("./messages.json"),
+    messages = require("./messages.json").messages;
 
-function writeMessagesFile(messages) {
-    fs.writeFileSync("./messages.json", JSON.stringify(messages, null, 4));
+function writeMessagesFile() {
+    fs.writeFileSync("./messages.json", JSON.stringify(message_body, null, 4));
 }
 
 function handleErrorMessage(res, messageId) {
     res.status(404).json({ msg: `id ${messageId} does not exist` });
 }
 
-//CRUD
-//Get:获取所有的留言 R：READ
+//Get:获取所有的留言 R:READ
 router.get("/", (req, res) => res.status(200).json(messages));
 
 // 获取单条留言
@@ -23,25 +22,25 @@ router.get("/:id", (req, res) => {
     message ? res.json(message) : handleErrorMessage(res, messageId);
 });
 
-//POST:添加留言 C: CREATE
+//POST:添加留言 C:CREATE
 router.post("/", (req, res) => {
     const newMessage = {
-        id: messages.length + 1,
+        id: messages.length,
         name: req.body.name,
         message: req.body.message,
     };
     messages.push(newMessage);
-    writeMessagesFile(message_body);
+    writeMessagesFile();
     res.status(201).json({ message: "create successfully", data: newMessage });
 });
 
-//PUT: 更新留言 U：UPDATE
+//PUT: 更新留言 U:UPDATE
 router.put("/:id", (req, res) => {
     const messageId = parseInt(req.params.id);
     const messageToUpdate = messages.find(msg => msg.id === messageId);
     if (messageToUpdate) {
         Object.assign(messageToUpdate, req.body);
-        writeMessagesFile(message_body);
+        writeMessagesFile();
         res.status(201).json({ message: "update successfully", data: messageToUpdate });
     } else handleErrorMessage(res, messageId);
 });
@@ -52,8 +51,8 @@ router.delete("/:id", (req, res) => {
     const messageIndex = messages.findIndex(msg => msg.id === messageId);
     if (messageIndex !== -1) {
         const deletedMessage = messages.splice(messageIndex, 1)[0];
-        writeMessagesFile(message_body);
-        res.json(deletedMessage);
+        writeMessagesFile();
+        res.status(201).json({ message: "delete successfully", data: deletedMessage });
     } else handleErrorMessage(res, messageId);
 });
 
--- Node_tutorial_code/server.js
+++ Node_tutorial_code/server.js
@@ -1,14 +1,14 @@
-//1.导入express
+//1.导入express, cors, route.js的express.Router()对象
 const express = require("express");
 const cors = require("cors");
+const messageRouter = require("./router");
 //2.创建一个web服务器
 const app = express();
-const messageRouter = require("./router");
-//允许postman的body使用json和x-www-form-urlencoded格式
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 //设置cors请求头 - 允许跨域请求
 app.use(cors());
+//允许postman在body使用json和x-www-form-urlencoded格式
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 //4.处理一个简单的request
 app.get("/test", (req, res) => res.send("hello world"));
 app.use(messageRouter);
